a334de73d696382952f99fd2468f78a222aa3767
Gustavo <gustavo.banin@hotmail.com>
2020-04-09 15:02:52 -0300

    Refresh token

--- src/main/java/com/example/cursomc/config/SecurityConfig.java
+++ src/main/java/com/example/cursomc/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
